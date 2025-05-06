856063e6cceec0a62b4e677593a6475c92744d5e
Paulo Sergio <pauloserg861@gmail.com>
2025-05-05 22:31:33 -0300

    test: ensure SignUpController calls AddSignUp with correct values

--- src/modules/signup/presentation/controller/signup.spec.ts
+++ src/modules/signup/presentation/controller/signup.spec.ts
@@ -1,14 +1,45 @@
+import { AddSignUpModel } from "../../domain/models/add-signup";
+import { AddSignUp } from "../../domain/usecases/add-signup";
 import { MissingParamError } from "../errors/missing-param-error";
+import { HttpRequest } from "../protocols/http";
 import { SignUpController } from "./signup";
 
-const makeSut = () => {
-  const sut = new SignUpController();
+const makeSignUp = (): AddSignUp => {
+  class AddSignUpStub implements AddSignUp {
+    async add(data: AddSignUpModel.Params): Promise<AddSignUpModel.Result> {
+      return new Promise((resolve) =>
+        resolve({
+          id: "valid_id",
+          name: "valid_name",
+          email: "valid_email@mail.com",
+          avatar: null,
+          accountId: "valid_account_id",
+          createdAt: new Date("2025-01-01T00:00:00Z"),
+        }),
+      );
+    }
+  }
+
+  return new AddSignUpStub();
+};
+
+const makeSut = (): SutTypes => {
+  const addSignStub = makeSignUp();
+  const sut = new SignUpController(addSignStub);
   return {
     sut,
+    addSignStub,
   };
 };
 
+type SutTypes = {
+  sut: SignUpController;
+  addSignStub: AddSignUp;
+};
+
 describe("SignUpController", () => {
+  type HttpRequetHandler = HttpRequest<AddSignUpModel.Params>;
+
   it("should be defined", () => {
     const { sut } = makeSut();
 
@@ -26,7 +57,7 @@ describe("SignUpController", () => {
         passwordConfirmation: "any_password",
       },
     };
-    const httpResponse = sut.handle(request);
+    const httpResponse = sut.handle(request as HttpRequetHandler);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("name"));
   });
@@ -40,7 +71,7 @@ describe("SignUpController", () => {
         passwordConfirmation: "any_password",
       },
     };
-    const httpResponse = sut.handle(request);
+    const httpResponse = sut.handle(request as HttpRequetHandler);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("email"));
   });
@@ -54,7 +85,7 @@ describe("SignUpController", () => {
         passwordConfirmation: "any_password",
       },
     };
-    const httpResponse = sut.handle(request);
+    const httpResponse = sut.handle(request as HttpRequetHandler);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(new MissingParamError("password"));
   });
@@ -68,10 +99,30 @@ describe("SignUpController", () => {
         password: "any_password",
       },
     };
-    const httpResponse = sut.handle(request);
+    const httpResponse = sut.handle(request as HttpRequetHandler);
     expect(httpResponse.statusCode).toBe(400);
     expect(httpResponse.body).toEqual(
       new MissingParamError("passwordConfirmation"),
     );
+  });
+
+  it("should call AddSignUp with correct values", () => {
+    const { sut, addSignStub } = makeSut();
+    const addSpy = jest.spyOn(addSignStub, "add");
+    const request = {
+      body: {
+        name: "any_name",
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    sut.handle(request as HttpRequetHandler);
+    expect(addSpy).toHaveBeenCalledWith({
+      name: "any_name",
+      email: "any_email@mail.com",
+      password: "any_password",
+      passwordConfirmation: "any_password",
+    });
   });
 });
