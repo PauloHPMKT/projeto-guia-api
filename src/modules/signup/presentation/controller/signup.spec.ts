9acc18b9a627e8f5095026e7857b600290c291ce
Paulo Sergio <pauloserg861@gmail.com>
2025-05-05 21:24:35 -0300

    test: ensure SignupController to valid and instance of SignupController

--- src/modules/signup/presentation/controller/signup.spec.ts
+++ src/modules/signup/presentation/controller/signup.spec.ts
@@ -3,6 +3,9 @@ import { SignUpController } from "./signup";
 describe("SignUpController", () => {
   it("should be defined", () => {
     const sut = new SignUpController();
+
     expect(sut).toBeDefined();
+    expect(sut).toBeInstanceOf(SignUpController);
+    expect(sut).toBeTruthy();
   });
 });
