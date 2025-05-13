d163288504f2efd15037b999717ffa102dfd3fdb
Lucas Pereira <DevLucasJP@gmail.com>
2025-05-12 21:46:49 -0300

    test: ensure user entity is defined

--- src/modules/signup/domain/entities/User.spec.ts
+++ src/modules/signup/domain/entities/User.spec.ts
@@ -1,12 +1,26 @@
 import { User } from "./User";
 
-describe("User Entity", () => {
-  it("shoud create an User", () => {
-    const newUser = new User("Lucas", "LTeste@teste.com", "12345678");
+const makeSut = (): User => {
+  return new User({
+    name: "Lucas",
+    email: "LTest@test.com",
+    password: "12345678",
+    accountId: "asdasdasdasdasdasd",
+  });
+};
 
-    expect(newUser.name).toBe("Lucas");
-    expect(newUser.email).toBe("LTeste@teste.com");
-    expect(newUser.accountId).toBeDefined();
-    expect(newUser.createdAt).toBeInstanceOf(Date);
+describe("User Entity", () => {
+  it("should be defined", () => {
+    const sut = makeSut();
+
+    expect(sut).toBeDefined();
+  });
+
+  it("shoud create an User", () => {
+    const sut = makeSut();
+
+    expect(sut.name).toBe("Lucas");
+    expect(sut.email).toBe("LTest@test.com");
+    expect(sut.createdAt).toBeInstanceOf(Date);
   });
 });
