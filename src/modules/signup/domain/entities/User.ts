91657b6a603e80acb12885a3a5ba0ad932ca8548
Lucas Pereira <DevLucasJP@gmail.com>
2025-05-12 21:46:20 -0300

    refactor: ensure user entity is defined

--- src/modules/signup/domain/entities/User.ts
+++ src/modules/signup/domain/entities/User.ts
@@ -1,18 +1,31 @@
-import { v4 } from "uuid";
+import { randomBytes } from "crypto";
 
 export class User {
+  public readonly id: string;
   public name: string;
   public email: string;
   public password: string;
-  public avatar: null;
-  public accountId: string;
-  public createdAt: Date;
+  public accountId: string | null;
+  public avatar?: string | null;
+  public createdAt?: Date;
 
-  constructor(name: string, email: string, password: string) {
-    this.name = name;
-    this.email = email;
-    this.password = password;
-    this.accountId = v4();
-    this.createdAt = new Date();
+  constructor(params: Omit<User, "id">) {
+    Object.assign(this, params);
+
+    this.id = randomBytes(12).toString("hex");
+    this.name = params.name;
+    this.email = params.email;
+    this.password = params.password;
+    this.accountId = params.accountId ?? null;
+    this.avatar = params.avatar ?? null;
+    this.createdAt = params.createdAt ?? new Date();
   }
 }
+
+const user = new User({
+  name: "Lucas",
+  email: "LTest@test.com",
+  password: "12345678",
+  accountId: "asdasdasdasdasdasd",
+});
+console.log(user);
